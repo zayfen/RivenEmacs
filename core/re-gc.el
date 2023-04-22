b5b124776c15619d28aa43570472c00e8e7bad36
zayfen <zhangyunfeng@pudutech.com>
2023-04-22 12:20:22 +0800

    chore: commit code

--- core/re-gc.el
+++ core/re-gc.el
@@ -14,11 +14,11 @@
   ;; 1s on my machine. Setting the factor to a higher value should solve the
   ;; issue on my machine, but I don't think it is right to assume it will work
   ;; the same way on other machines. So we switch back to a fixed delay of 20s.
-  (gcmh-idle-delay 20)
+  (gcmh-idle-delay 30)
   ;; The default `gcmh's 1GB is probably too high. We set it to 256MB on 64bit
   ;; systems, or 16MB on 32bit ones.
   (gcmh-high-cons-threshold
-   (* 16 1024 (if (string-suffix-p "64" (symbol-name sys/arch)) 1024 512))))
+   (* 32 1024 (if (string-suffix-p "64" (symbol-name sys/arch)) 1024 512))))
 
 
 (provide 're-gc)
