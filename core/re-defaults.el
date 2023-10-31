efb57d9c946210a50df34bdc1447778a3ea919cc
zayfen <zhangyunfeng@pudutech.com>
2023-10-31 22:35:07 +0800

    chore: update confitg

--- core/re-defaults.el
+++ core/re-defaults.el
@@ -446,6 +446,7 @@ or file path may exist now."
 
   ;; Auto load files changed on disk
   (global-auto-revert-mode 1)
+  (auto-revert-use-notify nil)
 
   ;; Show line number in mode-line
   (line-number-mode 1)
