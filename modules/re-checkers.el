94b3540164977cb8fa98af4bea342918dba835d8
zayfen <zhangyunfeng0101@gmail.com>
2023-12-22 23:06:04 +0800

    chore: commit

--- modules/re-checkers.el
+++ modules/re-checkers.el
@@ -52,4 +52,35 @@
   :hook ((rust-mode . flycheck-rust-setup)
          (rust-ts-mode . flycheck-rust-setup)))
 
+(flycheck-define-checker python-ruff
+  "A Python syntax and style checker using the ruff utility.
+To override the path to the ruff executable, set
+`flycheck-python-ruff-executable'.
+See URL `http://pypi.python.org/pypi/ruff'."
+  :command ("ruff"
+            "check"
+            (eval (when buffer-file-name
+                    (concat "" buffer-file-name)))
+            "-q")
+  :standard-input t
+  :error-filter (lambda (errors)
+                  (let ((errors (flycheck-sanitize-errors errors)))
+                    (seq-map #'flycheck-flake8-fix-error-level errors)))
+  :error-patterns
+  ((warning line-start
+            (file-name) ":" line ":" (optional column ":") " "
+            (id (one-or-more (any alpha)) (one-or-more digit)) " "
+            (message (one-or-more not-newline))
+            line-end))
+  :modes (python-ts-mode))
+
+;; Use something adapted to your config to add `python-ruff' to `flycheck-checkers'
+;; This is an MVP example:
+(setq python-ts-mode-hook
+      (list (defun my-python-hook ()
+              (unless (bound-and-true-p org-src-mode)
+                (when (buffer-file-name)
+                  (setq-local flycheck-checkers '(python-ruff))
+                  (flycheck-mode))))))
+
 (provide 're-checkers)
