81ad1f63cac4eff2e8ae3140f02eac903c1c8a30
zhangyunfeng <zhangyunfeng@pudutech.com>
2024-02-02 10:52:46 +0800

    chore: use super save

--- init.el
+++ init.el
@@ -244,3 +244,15 @@
   (+shutup! (native-compile-prune-cache)))
 
 (+log! "Loaded init.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
