c9c03cbf458fa398b59079991e18bd1951f5bc23
zayfen <zhangyunfeng0101@gmail.com>
2024-03-20 23:12:21 +0800

    chore: change font

--- core/re-font.el
+++ core/re-font.el
@@ -10,7 +10,7 @@
  ((string-equal system-type "darwin") ; macOS
   
   (when (member "Menlo" (font-family-list))
-    (set-face-attribute 'default nil :font "BlexMono Nerd Font Mono" :height 140)
+    (set-face-attribute 'default nil :font "BlexMono Nerd Font Mono" :height 160)
     (set-face-attribute 'fixed-pitch nil :font "Iosevka SS15")
     (set-face-attribute 'variable-pitch nil :font "Iosevka SS15")
     )
