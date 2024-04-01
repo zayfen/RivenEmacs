9daf46c37ed428783fd1ceae2a96acd4445a9e73
zayfen <zhangyunfeng0101@gmail.com>
2024-04-01 21:27:13 +0800

    chore: update config

--- modules/re-autosave.el
+++ modules/re-autosave.el
@@ -39,11 +39,10 @@
          (text-mode . super-save-mode))
   :custom
   (super-save-auto-save-when-idle t)
-  (super-save-idle-duration 2)
+  (super-save-idle-duration 1.3)
   (super-save-silent t)
   (super-save-delete-trailing-whitespace 'except-current-line)
   (super-save-exclude '(".gpg"))
   :config
   (add-to-list 'super-save-triggers 'ace-window)
   (add-to-list 'super-save-hook-triggers 'find-file-hook))
-
