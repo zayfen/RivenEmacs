0b0275ec4a7958dbba4c3e93eba2a3dbb526cf86
zayfen <zhangyunfeng0101@gmail.com>
2024-04-02 00:48:45 +0800

    chore: update font

--- core/re-font.el
+++ core/re-font.el
@@ -6,8 +6,14 @@
 ;;; Code:
 
 
-(set-face-attribute 'fixed-pitch nil :font "Iosevka SS15")
-(set-face-attribute 'variable-pitch nil :font "Iosevka SS15")
+(set-face-attribute 'fixed-pitch nil :font (font-spec :family "Iosevka SS15"
+                                                      :size (cond ((eq system-type 'gnu/linux) 12.5)
+                                                                  ((eq system-type 'windows-nt) 12.5)
+                                                                  ((eq system-type 'darwin) 18.0))))
+(set-face-attribute 'variable-pitch nil :font (font-spec :family "Iosevka SS15"
+                                                         :size (cond ((eq system-type 'gnu/linux) 12.5)
+                                                                     ((eq system-type 'windows-nt) 12.5)
+                                                                     ((eq system-type 'darwin) 18.0))))
 
 (defun font-installed-p (font-name)
   "Check if font with FONT-NAME is available."
@@ -27,37 +33,39 @@
                                                 ((eq system-type 'windows-nt) 12.5)
                                                 ((eq system-type 'darwin) 18)))))
 
-           (cl-loop for font in '("OpenSansEmoji" "Noto Color Emoji" "Segoe UI Emoji"
-                                  "EmojiOne Color" "Apple Color Emoji" "Symbola" "Symbol")
-                    when (font-installed-p font)
-                    return (set-fontset-font t 'unicode
-                                             (font-spec :family font
-                                                        :size (cond ((eq system-type 'gnu/linux) 16.5)
-                                                                    ((eq system-type 'windows-nt) 15.0)
-                                                                    ((eq system-type 'darwin) 18.0)))
-                                             nil 'prepend))
-           (cl-loop for font in '("思源黑体 CN" "思源宋体 CN" "微软雅黑 CN"
-                                  "Source Han Sans CN" "Source Han Serif CN"
-                                  "WenQuanYi Micro Hei" "文泉驿等宽微米黑"
-                                  "Microsoft Yahei UI" "Microsoft Yahei")
-                    when (font-installed-p font)
-                    return (set-fontset-font t '(#x4e00 . #x9fff)
-                                             (font-spec :name font
-                                                        :weight 'normal
-                                                        :slant 'normal
-                                                        :size (cond ((eq system-type 'gnu/linux) 12.8)
-                                                                    ((eq system-type 'windows-nt) 15.0)
-                                                                    ((eq system-type 'darwin) 18.0)
-                                                                    ))))
-           (cl-loop for font in '("HanaMinB" "SimSun-ExtB")
-                    when (font-installed-p font)
-                    return (set-fontset-font t '(#x20000 . #x2A6DF)
-                                             (font-spec :name font
-                                                        :weight 'normal
-                                                        :slant 'normal
-                                                        :size (cond ((eq system-type 'gnu/linux) 16.5)
-                                                                    ((eq system-type 'windows-nt) 15.0)
-                                                                    ((eq system-type 'darwin) 15.0))))))
+  (cl-loop for font in '("OpenSansEmoji" "Noto Color Emoji" "Segoe UI Emoji"
+                         "EmojiOne Color" "Apple Color Emoji" "Symbola" "Symbol")
+           when (font-installed-p font)
+           return (set-fontset-font t 'unicode
+                                    (font-spec :family font
+                                               :size (cond ((eq system-type 'gnu/linux) 16.5)
+                                                           ((eq system-type 'windows-nt) 15.0)
+                                                           ((eq system-type 'darwin) 18.0)))
+                                    nil 'prepend))
+  
+  (cl-loop for font in '("思源黑体 CN" "思源宋体 CN" "微软雅黑 CN"
+                         "Source Han Sans CN" "Source Han Serif CN"
+                         "WenQuanYi Micro Hei" "文泉驿等宽微米黑"
+                         "Microsoft Yahei UI" "Microsoft Yahei")
+           when (font-installed-p font)
+           return (set-fontset-font t '(#x4e00 . #x9fff)
+                                    (font-spec :name font
+                                               :weight 'normal
+                                               :slant 'normal
+                                               :size (cond ((eq system-type 'gnu/linux) 12.8)
+                                                           ((eq system-type 'windows-nt) 15.0)
+                                                           ((eq system-type 'darwin) 12.5)
+                                                           ))))
+
+  (cl-loop for font in '("HanaMinB" "SimSun-ExtB")
+           when (font-installed-p font)
+           return (set-fontset-font t '(#x20000 . #x2A6DF)
+                                    (font-spec :name font
+                                               :weight 'normal
+                                               :slant 'normal
+                                               :size (cond ((eq system-type 'gnu/linux) 16.5)
+                                                           ((eq system-type 'windows-nt) 15.0)
+                                                           ((eq system-type 'darwin) 14.0))))))
 
 
-  (provide 're-font)
+(provide 're-font)
