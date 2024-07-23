7bd38353d4c0d70da9a80840bc1688d586e71e32
zayfen <zhangyunfeng0101@gmail.com>
2024-07-23 13:06:41 +0800

    wip: commit

--- init.el
+++ init.el
@@ -43,7 +43,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(general which-key)))
+ '(package-selected-packages '(vc-use-package general which-key))
+ '(package-vc-selected-packages
+   '((vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-general.el
+++ lisp/init-general.el
@@ -1,7 +1,7 @@
 
 
 (use-package general
-  :vc (:fetcher "github" :repo "noctuid/general.el")
+  :vc (:fetcher github :repo noctuid/general.el)
   :config
   (setq general-keymap-global-map global-map) ; Set the keymap for general.el as global-map
   ; Other configurations specific to your needs...
