234258bbed7e18d6a25ffa2979b9c206a9f3e670
zayfen <zhangyunfeng0101@gmail.com>
2024-08-06 03:01:11 +0800

    chore: update config

--- lisp/init-pair.el
+++ lisp/init-pair.el
@@ -4,7 +4,14 @@
   :ensure smartparens  ;; install the package
   :hook (prog-mode text-mode markdown-mode) ;; add `smartparens-mode` to these hooks
   :config
-  ;; load default config
+  (defun create-newline-and-enter-sexp (&rest _ignored)
+    "Open a new brace or bracket expression, with relevant newlines and indent. "
+    (newline)
+    (indent-according-to-mode)
+    (forward-line -1)
+    (indent-according-to-mode))
+  (sp-local-pair 'tsx-ts-mode "{" nil :post-handlers '((create-newline-and-enter-sexp "RET")))
+
   (require 'smartparens-config))
 
 (provide 'init-pair)
