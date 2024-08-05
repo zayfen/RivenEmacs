58a070d24cc742f1bac9c92cf6a1d4379ae39014
zayfen <zhangyunfeng0101@gmail.com>
2024-08-06 02:35:34 +0800

    feat: install divrish

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -80,15 +80,48 @@
   :config
   (hl-todo-mode))
 
+;; issuse on MacOs
+;; brew install coreutils
 (use-package dirvish
   :ensure t
+  :init
+  (dirvish-override-dired-mode)
   :custom
-  (dirvish-attributes '(subtree-state all-the-icons file-size vc-state git-msg))
+  (dirvish-attributes '(subtree-state collapse file-size vc-state git-msg))
   (dirvish-mode-line-format '(:left (sort file-time symlink) :right (omit yank index)))
   (dirvish-side-width 30)
   (dirvish-fd-default-dir "~/")
   (dirvish-use-header-line t) ; 'global make header line span all panes
   (dirvish-use-mode-line t)
+  :config
+  ;; (dirvish-peek-mode) ; Preview files in minibuffer
+  ;; (dirvish-side-follow-mode) ; similar to `treemacs-follow-mode'
+  (setq delete-by-moving-to-trash t)
+  (setq dired-listing-switches
+        "-l --almost-all --human-readable --group-directories-first --no-group")
+  (setq dired-mouse-drag-files t)
+  (setq mouse-drag-and-drop-region-cross-program t)
+  (if os/mac (
+                (setq insert-directory-program "gls"))
+  :bind
+  (:map dirvish-mode-map ; Dirvish inherits `dired-mode-map'
+   ("a"   . dirvish-quick-access)
+   ("f"   . dirvish-file-info-menu)
+   ("y"   . dirvish-yank-menu)
+   ("N"   . dirvish-narrow)
+   ("^"   . dirvish-history-last)
+   ("h"   . dirvish-history-jump) ; remapped `describe-mode'
+   ("s"   . dirvish-quicksort)    ; remapped `dired-sort-toggle-or-edit'
+   ("v"   . dirvish-vc-menu)      ; remapped `dired-view-file'
+   ("TAB" . dirvish-subtree-toggle)
+   ("M-f" . dirvish-history-go-forward)
+   ("M-b" . dirvish-history-go-backward)
+   ("M-l" . dirvish-ls-switches-menu)
+   ("M-m" . dirvish-mark-menu)
+   ("M-t" . dirvish-layout-toggle)
+   ("M-s" . dirvish-setup-menu)
+   ("M-e" . dirvish-emerge-menu)
+   ("M-j" . dirvish-fd-jump))
 )
 
 (provide 'init-editor)
