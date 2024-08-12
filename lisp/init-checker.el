f28f13bf1701ae7dfe7d10effd0a765fb2de84e1
zhangyunfeng <zhangyunfeng@pudutech.com>
2024-08-12 13:53:24 +0800

    chore: use eslint from  node_modules

--- lisp/init-checker.el
+++ lisp/init-checker.el
@@ -63,6 +63,19 @@
 (when (executable-find "oxlint")
   (setq flycheck-javascript-eslint-executable "oxlint"))
 
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint
+          (and root
+               (expand-file-name "node_modules/.bin/eslint"
+                                 root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
 
 
 (provide 'init-checker)
