cd8995abb4861653e90094d3628f20d866a77a49
zhangyunfeng <zhangyunfeng@pudutech.com>
2024-08-15 10:22:59 +0800

    chore: update config

--- init.el
+++ init.el
@@ -5,7 +5,10 @@
   :demand t
   :if (memq window-system '(mac ns x))
   :config
+  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "NIX_SSL_CERT_FILE" "NIX_PATH" "GROQ_API_KEY"))
+    (add-to-list 'exec-path-from-shell-variables var))
   (exec-path-from-shell-initialize))
+
 
 (require 'init-use-package)
 (require 'init-default)
