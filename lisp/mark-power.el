24f4aa3a12fb0dca45988ce6a0f27a336dc764bc
zayfen <zhangyunfeng0101@gmail.com>
2024-09-27 02:08:08 +0800

    chore: update config

--- lisp/mark-power.el
+++ lisp/mark-power.el
@@ -2,35 +2,36 @@
 
 ;; combine with lsp-bridge for better jump
 
-(defconst HISTORY-MAX-SIZE 20)
+(defconst MARK-POWER--HISTORY-MAX-SIZE 20)
 
-(setq mark-enum '(("default" . 0)
+(setq mark-power--mark-enum '(("default" . 0)
 		              ("find-def" . 1)
 		              ("find-impl" . 2)
 		              ("find-typedef" . 3)
 		              ("find-peek" . 4)
 		              ))
 
-(setq mark-to-jump-back-handler '(("default" . xref-go-back)
+(setq mark-power--mark-to-jump-back-handler '(("default" . xref-go-back)
 			                  ("find-def" . lsp-bridge-find-def-return)
 			                  ("find-impl" . xref-go-back)
+                        ("find-typedef" . xref-go-back)
 			                  ("find-peek" . lsp-bridge-peek-jump-back)))
 
 (defun get-mark-value (mark-key)
   "return value of enum whoes key is {mark-key}"
-  (cdr (assoc mark-key mark-enum))
+  (cdr (assoc mark-key mark-power--mark-enum))
   )
 
 (defun get-mark-key (mark-value)
   "return key of enum whoes value is {mark-value}"
-  (let ((mark-key (car (rassoc mark-value mark-enum))))
+  (let ((mark-key (car (rassoc mark-value mark-power--mark-enum))))
     (if mark-key mark-key
       "default")
     ))
 
 (defun call-mark-jump-back-handler (mark-key)
   "get handler map to {mark}, and call it"
-  (let ((fn (assoc-default mark-key mark-to-jump-back-handler)))
+  (let ((fn (assoc-default mark-key mark-power--mark-to-jump-back-handler)))
     (funcall fn)))
 
 (setq mark-history '())
@@ -38,7 +39,7 @@
 (defun mark-power--set-mark (type)
   "push a mark to mark-history"
   (let ((mark-value (get-mark-value type)))
-    (if (>= (length mark-history) HISTORY-MAX-SIZE)
+    (if (>= (length mark-history) MARK-POWER--HISTORY-MAX-SIZE)
         (progn
 	        (pop mark-history)
 	        (setq mark-history (cons mark-value mark-history)))
