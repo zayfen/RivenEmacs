59571f55c45825caf025dd9ef2067a4d9b885a78
zayfen <zhangyunfeng0101@gmail.com>
2024-10-09 22:29:25 +0800

    chore: update config

--- custom.el
+++ custom.el
@@ -5,7 +5,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(ace-window aggressive-indent-mode blink-search color-rg combobulate crux dape dashboard devdocs diff-hl dirvish docker
+                dockerfile-mode doom-modeline elfeed-goodies embark-consult esup exec-path-from-shell expand-region
+                flycheck format-all general git-modes git-timemachine goto-last-change gpt-extensions.el hydra iedit
+                jtsx ligature lsp-bridge magit-todos marginalia orderless powerthesaurus prescient quickrun
+                rainbow-delimiters smartparens sudo-edit super-save symbol-overlay treesitter-context undo-fu
+                undo-fu-session vc-use-package vertico visual-regexp vscode-icon vundo web-mode yasnippet-snippets))
  '(package-vc-selected-packages
    '((elfeed-goodies :vc-backend Git :url "https://github.com/jeetelongname/elfeed-goodies")
      (elfeed :vc-backend Git :url "https://github.com/skeeto/elfeed")
