bb9fab53b763f7cc0e6ae9c2fc520a1d06e8bdbd
zayfen <zhangyunfeng0101@gmail.com>
2025-03-25 10:22:36 +0800

    fix: rust config

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -5,7 +5,7 @@
   :ensure t
   :mode ("\\.rs$\'" . rust-ts-mode)
   :init
-  (setq rust-mode-treesitter-derive t)
+  (setq rust-ts-mode-treesitter-derive t)
   :config
   (setq rust-ts-mode-indent-offset 2))
 
@@ -13,18 +13,18 @@
 ;;   :ensure t
 ;;   :after (rust-ts-mode)
 ;;   :config
-;;   (setq rustic-lsp-client nil)          ; avaiable values are: lsp eglot
+;;   (setq rustic-lsp-client nil)          ; available values are: lsp eglot
 ;;   (setq rustic-format-on-save nil)
 ;;   :custom
-;;   (rustic-analyzer-command '("rustup" "run" "stable" "rust-analyzer")
+;;   (rustic-analyzer-command '("rustup" "run" "stable" "rust-analyzer"))
 ;;   (rustic-cargo-use-last-stored-arguments t))
 
 (use-package cargo
   :hook (rust-ts-mode . cargo-minor-mode))
 
 (use-package flycheck-rust
-  :after (flycheck-mode)
-  :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+  :after (flycheck)
+  :hook (rust-ts-mode . flycheck-rust-setup))
 
 (provide 'init-rust)
 
