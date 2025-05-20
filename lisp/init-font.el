ccd44b24e2e4518c708c18576efa857d0d3a62fe
zhangyunfeng <zhangyunfeng@pudutech.com>
2025-05-20 20:37:31 +0800

    chore: change font

--- lisp/init-font.el
+++ lisp/init-font.el
@@ -6,13 +6,7 @@
 
 ;;; Code:
 
-
-(set-face-attribute 'default nil
-                   :font (font-spec :family "Liga SFMono Nerd Font"
-                                    :weight 'regular
-                                    :size (cond ((eq system-type 'gnu/linux) 13)
-                                                ((eq system-type 'windows-nt) 12)
-                                                ((eq system-type 'darwin) 14))))
+(set-face-attribute 'default nil :height 120)
 
 (set-face-attribute 'fixed-pitch nil
                     :font (font-spec :family "Liga SFMono Nerd Font"
@@ -32,35 +26,35 @@
   (find-font (font-spec :name font-name)))
 
 (when (display-graphic-p)
-  (cl-loop for font in '("Liga SFMono Nerd Font" "JetBrains Mono" "FiraCode Nerd Font" 
+  (cl-loop for font in '("Liga SFMono Nerd Font" "JetBrains Mono" "FiraCode Nerd Font"
                        "Cascadia Code" "SF Mono" "Menlo" "Monaco" "Consolas")
            when (font-installed-p font)
            return (set-face-attribute
                    'default nil
                    :font (font-spec :family font
                                     :weight 'normal
-                                    :size (cond ((eq system-type 'gnu/linux) 20)
-                                                ((eq system-type 'windows-nt) 12.5)
-                                                ((eq system-type 'darwin) 16)))))
+                                    :scale (cond ((eq system-type 'gnu/linux) 1.2)
+                                                ((eq system-type 'windows-nt) 1)
+                                                ((eq system-type 'darwin) 1)))))
 
   (cl-loop for font in '("Apple Color Emoji" "Segoe UI Emoji" "Noto Color Emoji")
            when (font-installed-p font)
            return (set-fontset-font t 'unicode
                                     (font-spec :family font
-                                               :size (cond ((eq system-type 'gnu/linux) 14)
+                                               :size (cond ((eq system-type 'gnu/linux) 16)
                                                            ((eq system-type 'windows-nt) 15.0)
                                                            ((eq system-type 'darwin) 16.0)))
                                     nil 'prepend))
 
-  (cl-loop for font in '("PingFang SC" "Microsoft YaHei" "Noto Sans CJK SC" 
+  (cl-loop for font in '("Sarasa Mono SC" "PingFang SC" "Microsoft YaHei" "Noto Sans CJK SC"
                        "Source Han Sans SC" "WenQuanYi Micro Hei")
            when (font-installed-p font)
            return (set-fontset-font t 'han
                                    (font-spec :family font
-                                              :weight 'regular
-                                              :size (cond ((eq system-type 'gnu/linux) 16)
-                                                          ((eq system-type 'windows-nt) 14)
-                                                          ((eq system-type 'darwin) 15)))))
+                                              :scale (cond ((eq system-type 'gnu/linux) 1)
+                                                          ((eq system-type 'windows-nt) 1)
+                                                          ((eq system-type 'darwin) 1)))))
+
   (cl-loop for font in '("HanaMinB" "SimSun-ExtB")
            when (font-installed-p font)
            return (set-fontset-font t '(#x20000 . #x2A6DF)
