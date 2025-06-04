e30ac86be9f29a71b9499fd6fa64441557a9c72e
zhangyunfeng <zhangyunfeng@pudutech.com>
2025-06-04 18:51:19 +0800

    feat: relative to project

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -47,7 +47,7 @@
   :hook (after-init . doom-modeline-mode)
   :config
   (setq doom-modeline-spc-face-overrides `(list :family (face-attribute 'fixed-pitch :family)))
-  (setq doom-modeline-buffer-file-name-style 'relative-from-project)
+  (setq doom-modeline-buffer-file-name-style 'relative-to-project)
   ;; Don’t compact font caches during GC.
   (setq inhibit-compacting-font-caches t))
 
