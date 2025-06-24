de85840841d40b54421ed7392a026f97313419cf
zhangyunfeng <zhangyunfeng@pudutech.com>
2025-06-24 11:08:39 +0800

    feat: update consult-buffer-filter

--- lisp/init-consult.el
+++ lisp/init-consult.el
@@ -58,6 +58,8 @@
   (consult-buffer-filter '(
                            "\\` "
                            "\\`\\*dashboard\\*\\'"
+                           "\\`\\*Native-compile-Log\\*\\'"
+                           "\\`\\*Async-native-compile-log\\*\\'"
                            "\\`\\*flycheck-posframe-buffer\\*\\'"
                            "\\`\\*Messages\\*\\'"
                            "\\`\\*scratch\\*\\'"
