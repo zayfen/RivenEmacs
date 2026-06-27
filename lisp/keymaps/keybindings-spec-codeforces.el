3cada7d58429fe7b12fe2c3c81541056120baff3
zayfen <zhangyunfeng0101@gmail.com>
2026-06-28 02:27:16 +0800

    chore(codeforces): drop C-c f leader-key bindings
    codeforces commands (login/logout/browse-problems) are invoked via M-x;
    no leader-key bindings are registered.  Removes the codeforces keybinding
    spec file and its registration in keybindings-spec.el.

--- lisp/keymaps/keybindings-spec-codeforces.el
+++
@@ -1,9 +0,0 @@
-;;; keybindings-spec-codeforces.el --- Codeforces keybinding specs -*- lexical-binding: t; -*-
-
-(defvar riven/keybindings-codeforces-spec
-  '(("l" codeforces-login "Login")
-    ("L" codeforces-logout "Logout")
-    ("p" codeforces-browse-problems "Browse problems"))
-  "Declarative specs for Codeforces commands under `C-c f'.")
-
-(provide 'keybindings-spec-codeforces)
